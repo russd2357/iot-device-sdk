5f6559297d159240742962e6876ca2ddff33cb3c
de Pina <pinaruss@38f9d3534cf6.ant.amazon.com>
2019-09-24 15:55:23 -0500

    Added notification rule to check over threshold condition and alarm only then

--- myfunction-lambda.py
+++ myfunction-lambda.py
@@ -1,25 +1,37 @@
 from __future__ import print_function
-  
+
 import json
 import boto3
-  
+
 print('Loading function')
-  
+
 def lambda_handler(event, context):
-  
-    # Parse the JSON message 
+
+    # Parse the JSON message
     eventText = json.dumps(event)
-  
+
     # Print the parsed JSON message to the console; you can view this text in the Monitoring tab in the Lambda console or in the CloudWatch Logs console
     print('Received event: ', eventText)
-  
+
+    data = event.get('data')
+    brokenRotorSig = data.get('Broken rotor signature')
+    debugmsg = 'Broken rotor signature is {0}'
+    print(debugmsg.format(brokenRotorSig))
+
+
+
     # Create an SNS client.Since our demo needs SMS capability, need to set the region
     sns = boto3.client('sns', region_name='us-east-1')
-    
-    # Publish a message to the specified topic
-    response = sns.publish (
-      TopicArn = 'arn:aws:sns:us-east-1:474013637073:VasDemoNotificationTopic',
-      Message = eventText
-    )
-  
+
+    # Notification rule:  Over threshold alarm
+    #
+    if (brokenRotorSig > 0.00175):
+      # Publish a message to the specified topic
+      response = sns.publish (
+        TopicArn = 'arn:aws:sns:us-east-1:474013637073:VasDemoNotificationTopic',
+        Message = 'Broken rotor signature over threshold: {0}'.format(brokenRotorSig)
+      )
+    else:
+      response = 'No alarm'
+
     print(response)
